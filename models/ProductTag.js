fe65d962412dfddd6987c29fc6a2c2c7512f7a47
song-richard <rsong1993@gmail.com>
2024-02-28 13:16:56 -0800

    Added model for productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,21 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    //validations for id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //validations for product_id
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
