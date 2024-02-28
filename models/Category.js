daab15eac63b5acaa2b8304c5ab53e17941e39c0
song-richard <rsong1993@gmail.com>
2024-02-28 13:07:05 -0800

    Added category_name to category.js

--- models/Category.js
+++ models/Category.js
@@ -14,7 +14,10 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
-
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
