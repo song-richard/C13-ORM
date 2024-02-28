391a6e48509c92e982191347960fb72f563d029e
song-richard <rsong1993@gmail.com>
2024-02-28 12:42:27 -0800

    Added logic to accept content and add tgo DB

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put(content);
+  await tx.done;
+  console.log('Content added to database');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
