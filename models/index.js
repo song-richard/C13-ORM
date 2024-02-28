c8f900e486a99ac384909e13904f06de88bb0250
song-richard <rsong1993@gmail.com>
2024-02-28 13:08:34 -0800

    Added rest of associations in index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,11 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+//For associations
+Product.belongsTo(Category);
+Category.hasMany(Product);
+Product.belongsToMany(Tag, { through: ProductTag })
+Tag.belongsToMany(Product, { through: ProductTag })
 
 module.exports = {
   Product,
