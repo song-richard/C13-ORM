4920858c529d96bedc463c3f2eeb30a294c369a8
song-richard <rsong1993@gmail.com>
2024-02-28 12:48:17 -0800

    Added notes. Set logic to notify user they can add to home screen

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,16 @@
 const butInstall = document.getElementById('buttonInstall');
 
+//initialize deferredPrompt
 let deferredPrompt;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
+    //stashed evt to use later
+    deferredPrompt = event;
+    //update UI to notify user they can add to home screen
+    butInstall.style.display = 'block';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
