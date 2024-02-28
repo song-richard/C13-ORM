e4ba716853bcf78de91de9ccdd82b8bf2089a607
song-richard <rsong1993@gmail.com>
2024-02-28 12:46:36 -0800

    Initialized deferredPrompt

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,10 +1,11 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
